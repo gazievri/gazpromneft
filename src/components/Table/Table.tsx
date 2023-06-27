3a7191d5c6f22f32e535df137813ac0ccd035cf2
Ruslan Gaziev <grinrus@yandex.ru>
2023-06-27 04:30:58 +0300

    Основной функционал Таблицы

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,27 +1,36 @@
-import { Table as TableAntd, Button } from 'antd';
+import { useEffect, useState } from 'react';
+import { Table as TableAntd, Button, Divider, Typography, Space } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 
 interface DataType {
     key: React.Key;
     name: string;
-    age: number;
-    address: string;
-  }
+    quantity: number;
+    price: number;
+    currency: string;
+    deliveryDate: string;
+}
 
 export const Table: React.FC = () => {
+    const [selectedRows, setSelectedRows] = useState<DataType[]>([]);
+    const [total, setTotal] = useState(0)
 
     const dataSource = [
         {
             key: '1',
             name: 'Mike',
-            age: 32,
-            address: '10 Downing Street',
+            quantity: 32,
+            price: 1000,
+            currency: 'RUB',
+            deliveryDate: "01.02.2023"
         },
         {
             key: '2',
             name: 'John',
-            age: 42,
-            address: '10 Downing Street',
+            quantity: 40,
+            price: 1100,
+            currency: 'USD',
+            deliveryDate: "12.02.2023"
         },
     ];
 
@@ -31,11 +40,13 @@ export const Table: React.FC = () => {
             title: 'Имя',
             dataIndex: 'name',
             key: 'name',
+            sorter: (a, b) => a.name.localeCompare(b.name),
         },
         {
             title: 'Количество',
             dataIndex: 'quantity',
             key: 'quantity',
+            sorter: (a, b) => a.quantity - b.quantity,
         },
         {
             title: 'Дата доставки',
@@ -46,18 +57,42 @@ export const Table: React.FC = () => {
             title: 'Цена',
             dataIndex: 'price',
             key: 'price',
+            sorter: (a, b) => a.price - b.price,
         },
         {
             title: 'Валюта',
             dataIndex: 'currency',
             key: 'currency',
+            sorter: (a, b) => a.currency.localeCompare(b.currency),
         },
     ];
 
+    const rowSelection = {
+        onChange: (_selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
+            setSelectedRows(selectedRows)
+        },
+    };
+
+    const handleClick = () => {
+        alert('!!!')
+    }
+
+    useEffect(() => {
+        if (selectedRows.length !== 0) {
+            const sum = selectedRows.reduce((sum, row) => sum + row.quantity, 0);
+            setTotal(sum);
+        } else {
+            setTotal(0)
+        }
+    }, [selectedRows])
+
     return (
-        <div>
-            <TableAntd dataSource={dataSource} columns={columns} pagination={false} />
-            <Button type="primary">Аннулировать</Button>
-        </div>
+        <Space direction='vertical' style={{ display: 'flex' }}>
+            <TableAntd dataSource={dataSource} columns={columns} pagination={false} rowSelection={rowSelection} />
+            <Typography.Text strong>Общее количество: {total}</Typography.Text>
+            {
+                total > 0 && <Button type="primary" onClick={handleClick}>Аннулировать</Button>
+            }
+        </Space>
     )
 }
--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -8,8 +8,8 @@ export const Header: React.FC = () => {
                 <img className={styles.logo} src={logo} alt="Логотип компании Газпронефть" />
                 <nav>
                     <ul className={styles.list}>
-                        <li><a className={styles.link} href="" target='_blank'>Текст задания</a></li>
-                        <li><a className={styles.link} href="" target='_blank'>Репозиторий</a></li>
+                        <li><a className={styles.link} href="https://docs.google.com/document/d/1BCe2Ze4lg3JIc9A83Hh50hvFooVNU28ASZAsj-IbrUk/edit?usp=sharing" target='_blank'>Текст задания</a></li>
+                        <li><a className={styles.link} href="https://github.com/gazievri/gazpromneft" target='_blank'>Репозиторий</a></li>
                     </ul>
                 </nav>
             </div>
